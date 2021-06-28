2839fa39332531536f027c595cf6ead15c0e91b9
Aditya Papneja <apapneja@confederationcollege.ca>
2021-06-27 21:38:01 -0500

    Lab 2

---
+++ BenfordLab/FirstDigit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLab
+{
+    public class FirstDigit
+    {
+        /* Given an integer X, this method returns the first digit
+         * 
+         * For example,
+         *   5 ==> returns 5
+         *   10 ==> returns 1
+         *   654321 ==> returns 6
+         */
+        public static int getFirstDigit(int digit)
+        {
+            // TODO
+            int i = Math.Abs(digit);
+            while (i >= 10)
+                i /= 10;
+            return i;
+        }
+    }
+}
